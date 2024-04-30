1ec3e08e9c42e543852fa751316e73f0a18aa17d
Igor Gavrilchik <Lolopat1994@gmail.com>
2024-04-30 22:10:26 +0300

    new function title + docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_upper(string: str):
+    """Делае все буквы заглавными"""
     return string.upper()
+
+
+def get_title(string: str):
+    """Делает все слова с заглавной буквы"""
+    return string.title()
